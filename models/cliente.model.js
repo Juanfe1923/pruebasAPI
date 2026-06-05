82393242b113f81ab8e530f721a600c4e5b1fd4d
juanfe1923 <felipeuribe778@gmail.com>
2026-06-05 14:14:38 -0500

    cambios

--- models/cliente.model.js
+++ models/cliente.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const clienteSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  nombre: { type: String, required: [true, 'nombre obligatorio'] },
+  email: { type: String, required: [true,'email obligatorio'], unique: [true, 'email ya registrado'] },
   telefono: { type: String }
 });
 
--- models/servicio.model.js
+++ models/servicio.model.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const servicioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  precio: { type: Number, required: true },
-  duracionMinutos: { type: Number, required: true }
+  nombre: { type: String, required: [true , 'nombre obligatorio'] },
+  precio: { type: Number, required: [true, 'precio obligatorio'] },
+  duracionMinutos: { type: Number, required: [true, 'duracion obligatoria'] }
 });
 
 module.exports = mongoose.model('Servicio', servicioSchema, 'servicios');
